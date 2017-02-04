d2d8e18976c66849d8925209621e376ee1fcebc2
juanmam <juanmam941025@gmail.com>
2017-02-04 17:37:15 -0500

    Trying to solve the forbiden '/' problem, it most be somewhere in index.js, though it's a code that I'm not familiar with and is probing to be a pain.

--- index.js
+++ index.js
@@ -4,19 +4,13 @@
 
 var express = require('express');
 var app = express();
+var path = require("path");
 
 var port = 5000;
 var index = 'views/index.html';
 
-app.set('port',(process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request,response) {
-    response.render(index);
+app.get('/', function(req,res) {
+    res.render(path.join(__dirname + index));
 });
 
 app.listen(app.get('port'), function() {
