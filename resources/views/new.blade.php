aa2e31bfa3fffd80ccd6ce0945efa1eba27ad62f
juanmam941025 <juanmam941025@gmail.com>
2017-02-22 00:04:07 -0500

    Some changes to the standard project, just need to push so I can clone on a native ubuntu env.

---
+++ resources/views/new.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>New Project</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <link href="{{ URL::asset('css/general.css') }}" rel="stylesheet"/>
+  </head>	
+  <body>
+    <header>
+      <div class="container">
+	<div id="left">
+	  <strong>
+	    <a href="/">
+	      Home
+	    </a>
+	  </strong>
+	  <strong>
+	    <a href="">
+	    </a>
+	  </strong>
+	</div>
+	<div id="right">
+	  <strong>
+	    <a href="">
+	      Login
+	    </a>
+	  </strong>
+	</div>
+      </div>
+    </header>
+    <br>
+
+    
+  </body>
+</html>
